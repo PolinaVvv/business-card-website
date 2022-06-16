44c5305c481dab4d6a05cf2cdd1540f7537c8a6c
Ningaro <23503775+Ningaro@users.noreply.github.com>
2022-06-17 01:56:50 +0300

    📱 Close #5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './screens/App'
+import legsAndFire from './assets/imgs/legs_and_fire2.jpg'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
   fonts: {
     heading: `'Montserrat', sans-serif`,
     body: `'Montserrat', sans-serif`,
+  },
+  styles: {
+    global: {
+      body: {
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: legsAndFire,
+        backgroundSize: 'cover',
+        backgroundPositionX: '30%',
+      },
+    },
   },
 })
 
--- src/screens/App.js
+++ src/screens/App.js
@@ -1,77 +1,85 @@
 import {
   Button,
-  Image,
-  Box,
   Flex,
   Spacer,
   Heading,
   Text,
-  HStack,
+  Stack,
+  Center,
 } from '@chakra-ui/react'
-import legsAndFire from '../assets/imgs/legs_and_fire2.jpg'
 import '@fontsource/montserrat/400.css'
 
 function App() {
   return (
-    <div className="App">
-      <Flex>
-        <Box
-          backgroundSize="cover"
-          h="100vh"
-          w="100vw"
-          backgroundRepeat="no-repeat"
-          backgroundImage={legsAndFire}
+    <Flex
+      direction="column"
+      minH="100vh"
+      w="100vw"
+      px={{ base: '1em', lg: '4em' }}
+    >
+      <Stack
+        direction={{ base: 'column', lg: 'row' }}
+        flex="1"
+        color="rgba(202, 200, 185, 1)"
+        pt={{ lg: '4em' }}
+        spacing={{ base: '2em', lg: '2em' }}
+        justifyContent={{ base: 'normal', lg: 'space-between' }}
+      >
+        <Heading
+          mt={{ base: '1em', lg: '0' }}
+          fontWeight="800"
+          size="2xl"
+          textAlign={{ base: 'center' }}
         >
-          <Flex color="rgba(202, 200, 185, 1)" mt="2.5em">
-            <Heading fontWeight="800" size="2xl" pl="1.9em" pt="0.2em">
-              @naitiphoto
-            </Heading>
-            <Spacer />
-            <HStack fontWeight="300" spacing="3.8em" pr="3.3em">
-              <Text fontSize="2xl">О фотографе</Text>
-              <Text fontSize="2xl">Портфолио</Text>
-              <Text fontSize="2xl">Отзывы</Text>
-              <Text fontSize="2xl">Цены</Text>
-              <Text fontSize="2xl">Контакты</Text>
-            </HStack>
-          </Flex>
-          <Flex mt="9em" pl="5em">
-            <Button
-              colorScheme="teal"
-              variant="outline"
-              borderColor="rgba(143, 91, 55, 1)"
-              _hover={{ bg: 'rgba(143 91 55 / 30%)' }}
-              size="lg"
-              align="center"
-              padding="2em"
-              borderRadius="0em"
-              border="0.2em solid"
-              color="rgba(202, 200, 185, 1)"
-              fontWeight="500"
-              pos="absolute"
-              mt="2.9em"
-              margin="4em 3em 0em 3em"
-            >
-              Записаться на съёмку
-            </Button>
-            <Spacer />
-            <Heading
-              color="rgba(202, 200, 185, 1)"
-              fontWeight="800"
-              size="2xl"
-              pl="12.9em"
-              pt="0.2em"
-              pr="3em"
-              textAlign="start"
-              lineHeight="1.2"
-            >
-              В поисках фотографии я нахожу нечто более прекрасное, чем просто
-              мир
-            </Heading>
-          </Flex>
-        </Box>
-      </Flex>
-    </div>
+          @naitiphoto
+        </Heading>
+        <Stack
+          direction={{ base: 'column', lg: 'row' }}
+          fontWeight="300"
+          spacing={{ base: '0.5em', lg: '3.8em' }}
+        >
+          <Text fontSize="2xl">О фотографе</Text>
+          <Text fontSize="2xl">Портфолио</Text>
+          <Text fontSize="2xl">Отзывы</Text>
+          <Text fontSize="2xl">Цены</Text>
+          <Text fontSize="2xl">Контакты</Text>
+        </Stack>
+      </Stack>
+      <Stack
+        my={{ base: '1em', lg: '0' }}
+        alignItems={{ lg: 'center' }}
+        direction={{ base: 'column-reverse', lg: 'row' }}
+        spacing={{ base: '1em', lg: '0.5em' }}
+        flex="1"
+      >
+        <Center flex="1">
+          <Button
+            colorScheme="teal"
+            variant="outline"
+            borderColor="rgba(143, 91, 55, 1)"
+            _hover={{ bg: 'rgba(143 91 55 / 30%)' }}
+            size="lg"
+            borderRadius="0em"
+            border="0.2em solid"
+            color="rgba(202, 200, 185, 1)"
+            fontWeight="500"
+          >
+            Записаться на съёмку
+          </Button>
+        </Center>
+        <Heading
+          flex="3"
+          color="rgba(202, 200, 185, 1)"
+          fontWeight="800"
+          size="2xl"
+          textAlign="start"
+          lineHeight="1.2"
+        >
+          В поисках фотографии я нахожу нечто более прекрасное, чем просто мир
+        </Heading>
+      </Stack>
+      <Spacer />
+    </Flex>
   )
 }
 
